a9b87977c07e9b4f4cf3cc41dcda2c76838d390f
Elias <eliasful@gmail.com>
2018-11-30 12:39:59 -0200

    corrige lista

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -2,6 +2,9 @@ import Route from '@ember/routing/route';
 import { set } from '@ember/object';
 
 export default Route.extend({
+  afterModel() {
+    this.transitionTo('home');
+  },
   actions: {
     willTransition() {
       let application = this.controllerFor('application');
