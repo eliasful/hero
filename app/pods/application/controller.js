547e80a1dd7c2066fbc6dcfa179ecd78ae410489
Elias <elias@gitlab.viasoft.com.br:8288>
2018-11-30 09:43:47 -0200

    Altera busca e insere imagem

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -1,13 +1,26 @@
 import Controller from '@ember/controller';
 import { A } from '@ember/array';
+import { task, timeout } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  ajax: service(),
   heros: A(),
+  loading: false,
   search: null,
-  actions: {
-    // clearAndTransition() {
-    //   this.set('search', null);
-    //   this.transitionToRoute('hero.new');
-    // }
-  }
+  doSearch: task(function * (value) {
+    this.set('search', value);
+    yield timeout(500);
+    try {
+      if (!value) {
+        this.set('heros', A());
+        return;
+      }
+
+      let heros = yield this.get('ajax').request(`heros/?name=${value}`);
+      this.set('heros', heros);
+    } catch(e) {
+      alert(e);
+    }
+  }).restartable(),
 });
--- tests/integration/pods/components/card-image/component-test.js
+++ tests/integration/pods/components/card-image/component-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | input-search', function(hooks) {
+module('Integration | Component | card-image', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{input-search}}`);
+    await render(hbs`{{card-image}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#input-search}}
+      {{#card-image}}
         template block text
-      {{/input-search}}
+      {{/card-image}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
