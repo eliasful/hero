b6b4f7909a6655f3698ac501b26d9c1bf36a27e9
Elias <elias@gitlab.viasoft.com.br:8288>
2018-11-30 17:57:22 -0200

    inflection

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,13 @@
 import DRFAdapter from './drf';
+import { underscore } from '@ember/string';
+import { pluralize } from 'ember-inflector';
 
 export default DRFAdapter.extend({
+  pathForType(type) {
+    let underscored = underscore(type);
+    if (underscored === 'hero') {
+      underscored = 'heroes'
+    }
+    return pluralize(underscored);
+  }
 });
