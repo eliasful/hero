fd8c09dd73dd7b25aaeecad395694fcab52ea353
Elias <eliasful@gmail.com>
2018-12-01 12:03:38 -0200

    valida campos formulario

--- app/pods/components/super-heroes/form/component.js
+++ app/pods/components/super-heroes/form/component.js
@@ -1,16 +1,26 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import SweetAlertMixin from 'ember-sweetalert/mixins/sweetalert-mixin';
 
-export default Component.extend({
+export default Component.extend(SweetAlertMixin, {
   router: service(),
   tagName: '',
   actions: {
     async save(model) {
+      let sweetAlert = this.get('sweetAlert');
       try {
         await model.save();
         this.get('router').transitionTo('home');
+        sweetAlert('Sucesso!', 'Herói cadastrado com sucesso!', 'success');
       } catch(e) {
-        alert(e);
+        let errors = '';
+        e.errors.forEach(error => {
+          let split = error.source.pointer.split('/');
+          let field = split[split.length - 1];
+          let message = error.detail;
+          errors += `${field}: ${message}<br>`;
+        });
+        sweetAlert('Ops!', 'Não foi possível cadastrar o herói!<br>' + errors, 'warning');
       }
     },
     async destroy(model) {
